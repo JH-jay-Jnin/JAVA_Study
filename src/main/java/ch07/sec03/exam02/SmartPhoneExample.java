3396f3ecc52637c2bca02f027e3b2e8929a6c098
JH-jay-Jnin <jjustice551@gmail.com>
2026-04-21 17:47:59 +0900

    Feat. 실습문제 (기본) 풀다 말았음.

--- src/main/java/ch07/sec03/exam02/SmartPhoneExample.java
+++ src/main/java/ch07/sec03/exam02/SmartPhoneExample.java
@@ -1,4 +1,15 @@
+/* 다음의Phone 클래스를상속한SmartPhone클래스를정의하세요.
+   => model, color는 SmartPhone 클래스의 생성자 매개변수로 초기화 함
+
+   이어서, SmartPhoneExample 클래스로 SmartPhone의 인스턴스를 생성하여
+   초기화가 올바른지 확인하세요.
+ */
 package main.java.ch07.sec03.exam02;
 
 public class SmartPhoneExample {
+
+    public static void main(String[] args) {
+        // SmartPhone 인스턴스 생성
+        SmartPhone SmartEx = new SmartPhone("갤럭시", "은색");
+    }
 }
