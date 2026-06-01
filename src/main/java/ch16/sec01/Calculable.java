e8fa7abfe4587d648140ab6eaff96d7fc59bf601
JH-jay-Jnin <jjustice551@gmail.com>
2026-06-01 09:14:15 +0900

    Feat. 17 Input Output Practice

---
+++ src/main/java/ch16/sec01/Calculable.java
@@ -0,0 +1,8 @@
+// 1. 다음 인터페이스를 함수형 인터페이스로 명시하세요.
+
+package main.java.ch16.sec01;
+
+public interface Calculable {
+    // 추상 메소드
+    void calculate(int x, int y);
+}
---
+++ src/main/java/ch16/sec01/LambdaExample.java
@@ -0,0 +1,20 @@
+//// 2. 앞의 Calculator 인터페이스를 이용해서,
+////   다음 출력이 나오도록 하시오. ==> result 14 | result 6
+//
+//package main.java.ch16.sec01;
+//
+//public class LambdaExample {
+//    public static void main(String[] args) {
+//        // x + y 계산
+//        LambdaExample( x + y)
+//        // x - y 계산
+//    }
+//    public static void action(Calculable calculable) {
+//        // 데이터
+//        int x = 10;
+//        int y = 4;
+//        // 데이터 처리
+//        calculable.calculate(x,y);
+//    }
+//
+//}
